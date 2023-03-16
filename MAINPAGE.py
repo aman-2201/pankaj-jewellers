949058aade78953a9c133398ded307d00d92731b
anon22 <107742184+aman-2201@users.noreply.github.com>
2023-03-16 21:58:11 +0530

    Update MAINPAGE.py

--- MAINPAGE.py
+++ MAINPAGE.py
@@ -67,13 +67,12 @@ with open("pankajjewellers.jpg", "rb") as file:
             file_name="pankajjewellers.jpg",
             mime="image/jpg"
           )
-
- hide_streamlit_style = """
+hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)  
 
 
